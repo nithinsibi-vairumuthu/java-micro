e04de46c6e3c68c3b91ffa97f22a6d914732ea54
nithinsibi-vairumuthu <nithinsibi@gmail.com>
2025-12-22 19:45:52 +0530

    Create HelloController.java

---
+++ demo-app/src/main/java/com/devops/HelloController.java
@@ -0,0 +1,13 @@
+package com.devops;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello from DevOps CI/CD App 🚀";
+    }
+}
