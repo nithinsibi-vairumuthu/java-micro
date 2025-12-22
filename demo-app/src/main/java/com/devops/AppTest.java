df79b1c7012d88a46325f644d371a7594b08e943
nithinsibi-vairumuthu <nithinsibi@gmail.com>
2025-12-22 19:54:15 +0530

    Create AppTest.java

---
+++ demo-app/src/main/java/com/devops/AppTest.java
@@ -0,0 +1,13 @@
+package com.devops;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+
+    @Test
+    void sampleTest() {
+        // Simple test to make sure pipeline passes
+        assertTrue(true);
+    }
+}
