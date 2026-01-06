9f9b9544398bbec05e01b0cac9bf84a589ccf86a
nithin-vairumuthu <nithinsibi@gmail.com>
2026-01-06 16:08:28 +0530

    app modified

--- demo-app/src/main/java/com/devops/HelloController.java
+++ demo-app/src/main/java/com/devops/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello from DevOps CI/CD App 🚀";
+        return "Hello from DevOps App 🚀";
     }
 }
